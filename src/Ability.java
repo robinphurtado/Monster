7501d8fd7492a018ddac936e671068d2e661c1e9
Robin <rohurtado@csumb.edu>
2022-12-06 18:48:13 -0800

    update part 1

---
+++ src/Ability.java
@@ -0,0 +1,5 @@
+public interface Ability {
+
+    //this is a tagging interface.  This is all we know about it as of Hw 01
+
+}
---
+++ src/Attack.java
@@ -0,0 +1,9 @@
+public interface Attack extends Ability {
+
+    //this is an interface that is organized under the Ability interface
+    //That is all we know as of HW01
+
+    public abstract Integer attack(Monster target);
+
+
+}
